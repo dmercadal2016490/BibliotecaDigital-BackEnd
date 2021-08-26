8bf91ee81f96c288c7284289f63ec7337da56f09
Diego Mercadal <dmecadal-2016490@kinal.edu.gt>
2021-08-25 18:22:22 -0600

    Devolver libro / cancelar reservacion

--- BibliotecaDigital-BackEnd/controller/reservacion.controller.js
+++ BibliotecaDigital-BackEnd/controller/reservacion.controller.js
@@ -13,6 +13,7 @@ function reservar(req,res){
         User.findById(userId, (err, userFind)=>{
             if(err){
                 res.status(500).send({message: 'Error general al buscar al usuario'});
+                console.log(err);
             }else if(userFind){
                 if(userFind.librosRentados >= 10){
                     res.send({message: 'No puedes rentar mas de 10 libros'})
@@ -75,6 +76,63 @@ function reservar(req,res){
     }
 }
 
+function devolver(req,res){
+    var userId = req.params.idU;
+    var libroId = req.params.idL;
+
+    if(userId != req.user.sub){
+        res.status(403).send({message: 'No tienes permisos para reservar un libro'});
+    }else{
+        User.findById(userId, (err, userFind)=>{
+            if(err){
+                res.status(500).send({message: 'Error general al buscar al usuario'});
+                console.log(err);
+            }else if(userFind){
+                Libro.findById(libroId, (err, libroFind)=>{
+                    if(err){
+                        res.status(500).send({message: 'Error general al buscar el libro'});
+                        console.log(err);
+                    }else if(libroFind){
+                        Libro.findByIdAndUpdate(libroId, {$inc:{disponibles: +1}}, {new:true}, (err, libroPushed)=>{
+                            if(err){
+                                res.status(500).send({message: 'Error general al actualizar el libro'});
+                                console.log(err);
+                            }else if(libroPushed){
+                                User.findOneAndUpdate({_id: userId, Libros: libroId}, {$pull:{Libros: libroId}}, {new:true}, (err, userPulled)=>{
+                                    if(err){
+                                        res.status(500).send({message: 'Error general al devolver el libro'});
+                                        console.log(err);
+                                    }else if(userPulled){
+                                        User.findByIdAndUpdate(userId, {$inc:{librosRentados: -1}}, {new:true}, (err, devuelto)=>{
+                                            if(err){
+                                                res.status(500).send({message: 'Error general al actualizar al usuario'});
+                                                console.log(err);
+                                            }else if(devuelto){
+                                                res.send({message: 'Libro devuelto con exito ', devuelto});
+                                            }else{
+                                                res.send({message: 'No se actualizo el usuario'});
+                                            }
+                                        })
+                                    }else{
+                                        res.send({message: 'No se devolvio el libro'});
+                                    }
+                                })
+                            }else{
+                                res.status(401).send({message: 'No se actualizo el libro'});
+                            }
+                        })
+                    }else{
+                        res.status(404).send({message: 'El libro que quieres devolver no existe'});
+                    }
+                })
+            }else{
+                res.status(404).send({message: 'No se encontro al usuario'});
+            }
+        })
+    }
+}
+
 module.exports = {
-    reservar
+    reservar,
+    devolver
 }
--- BibliotecaDigital-BackEnd/routes/reservacion.routes.js
+++ BibliotecaDigital-BackEnd/routes/reservacion.routes.js
@@ -6,6 +6,7 @@ var mdAuth = require('../middlewares/authenticated');
 
 var api = express.Router();
 
-api.put('/:idU/reservar/:idL', mdAuth.ensureAuth, reservacionController.reservar)
+api.put('/:idU/reservar/:idL', mdAuth.ensureAuth, reservacionController.reservar);
+api.put('/:idU/devolver/:idL', mdAuth.ensureAuth, reservacionController.devolver);
 
 module.exports = api; 
